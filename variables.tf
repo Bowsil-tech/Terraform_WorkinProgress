bc12b0c73e3429e059a904bfb1578a4938d24f7e
Bowsil Ameen Surajudeen <bowsilameen@gmail.com>
2022-09-21 15:56:41 +0400

    Second commit

--- variables.tf
+++ variables.tf
@@ -1,2 +1,15 @@
 
 
+variable "resource_group_name" {
+  type = map(any)
+  default = {
+    rgnameingest        = "rg-terraform-ingest"
+    rgnameprocess       = "rg-terraform-process"
+    nsgnamesubnetingest = "nsg-terraform-ingest"
+    eventhubnamespace   = "ns-evh-terraform-edpppe"
+    acceptanceEventHub  = "sbr.vcr.ppe"
+  }
+}
+
+
+
